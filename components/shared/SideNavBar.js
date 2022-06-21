34ca35c3e811671bd0acd9538fa64f1d91c1ddcc
Marlon Fernandez <marlonfsolis@outlook.com>
2022-06-21 08:57:04 -0400

    Using react-bootstrap on side nav menu.

--- components/shared/SideNavBar.js
+++ components/shared/SideNavBar.js
@@ -1,5 +1,8 @@
 import { useState } from "react"
+import Image from "next/image"
+
 import SideNavbarMenuItems from "./SideNavbarMenuItems";
+
 import styles from "styles/Layout.module.css";
 
 export default function SideNavbar(props) {
@@ -43,42 +46,10 @@ export default function SideNavbar(props) {
 
       <hr />
       <SideNavbarMenuItems />
-      {/* <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item">
-          <a href="#" className="nav-link active" aria-current="page">
-            <i className="bi bi-house me-2" width="16" height="16">i</i>
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <i className="bi bi-speedometer2 me-2" width="16" height="16"></i>
-            Dashboard
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <i className="bi bi-table me-2" width="16" height="16"></i>
-            Orders
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <i className="bi bi-grid me-2" width="16" height="16"></i>
-            Products
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <i className="bi bi-person-circle me-2" width="16" height="16"></i>
-            Customers
-          </a>
-        </li>
-      </ul> */}
       <hr />
       <div className="dropdown">
         <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
+          <Image src="https://github.com/mdo.png" alt="" width={32} height={32} className="rounded-circle me-2" />
           <strong>mdo</strong>
         </a>
         <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
@@ -133,7 +104,7 @@ export default function SideNavbar(props) {
       </ul>
       <div className="dropdown border-top">
         <a href="#" className="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" className="rounded-circle" />
+          <Image src="https://github.com/mdo.png" alt="mdo" width={24} height={24} className="rounded-circle" />
         </a>
         <ul className="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
           <li><a className="dropdown-item" href="#">New project...</a></li>
--- components/shared/SideNavbarMenuItems.js
+++ components/shared/SideNavbarMenuItems.js
@@ -1,37 +1,15 @@
+
+import Nav from "react-bootstrap/Nav"
+
 export default function SideNavbarMenuItems() {
 
   return (
-    <ul className="nav nav-pills flex-column mb-auto">
-      <li className="nav-item">
-        <a href="#" className="nav-link active" aria-current="page">
-          <i className="bi bi-house me-2" width="16" height="16">i</i>
-          Home
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <i className="bi bi-speedometer2 me-2" width="16" height="16"></i>
-          Dashboard
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <i className="bi bi-table me-2" width="16" height="16"></i>
-          Orders
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <i className="bi bi-grid me-2" width="16" height="16"></i>
-          Products
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <i className="bi bi-person-circle me-2" width="16" height="16"></i>
-          Customers
-        </a>
-      </li>
-    </ul>
+    <Nav variant="pills" defaultActiveKey="/home" className="flex-column mb-auto">
+      <Nav.Link href="/home"><i className="bi bi-house me-2" width="16" height="16"></i> Home</Nav.Link>
+      <Nav.Link eventKey="/home/about"><i className="bi bi-info-square me-2" width="16" height="16"></i> About</Nav.Link>
+      <Nav.Link eventKey="link-2"><i className="bi bi-table me-2" width="16" height="16"></i> Orders</Nav.Link>
+      <Nav.Link eventKey="link-3"><i className="bi bi-grid me-2" width="16" height="16"></i> Products</Nav.Link>
+      <Nav.Link eventKey="link-4" disabled><i className="bi bi-person-circle me-2" width="16" height="16"></i> Customers</Nav.Link>
+    </Nav>
   )
 }
