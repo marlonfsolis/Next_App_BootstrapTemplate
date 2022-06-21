5337ff49017aa85a6164e1d4bec4138034acd290
Marlon Fernandez <marlonfsolis@outlook.com>
2022-06-21 07:00:06 -0400

    Adding the About page.

---
+++ pages/home/about.js
@@ -0,0 +1,13 @@
+import Layout from "components/shared/Layout";
+
+export default function About() {
+
+  return (
+    <Layout>
+      <main className="container">
+        <h1>About</h1>
+        <p>This is the About page.</p>
+      </main>
+    </Layout>
+  )
+}
