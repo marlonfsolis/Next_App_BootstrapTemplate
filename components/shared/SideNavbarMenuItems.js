502d4c2d4473b4b80abf626f92f5268918a20fb5
Marlon Fernandez <marlonfsolis@outlook.com>
2022-06-22 08:00:47 -0400

    Just replacing all Nav.Link with ActiveLink.

--- components/shared/SideNavbarMenuItems.js
+++ components/shared/SideNavbarMenuItems.js
@@ -8,10 +8,10 @@ export default function SideNavbarMenuItems() {
   return (
     <Nav variant="pills" defaultActiveKey="/home" className="flex-column mb-auto">
       <ActiveLink href="/home" type="navLink" exact={true}><i className="bi bi-house me-2" width="16" height="16"></i>Home</ActiveLink>
-      <ActiveLink href="/home/about" type="navLink" exact={true}><i className="bi bi-info-square me-2" width="16" height="16"></i> About</ActiveLink>
-      <Nav.Link eventKey="link-2"><i className="bi bi-table me-2" width="16" height="16"></i> Orders</Nav.Link>
-      <Nav.Link eventKey="link-3"><i className="bi bi-grid me-2" width="16" height="16"></i> Products</Nav.Link>
-      <Nav.Link eventKey="link-4" disabled><i className="bi bi-person-circle me-2" width="16" height="16"></i> Customers</Nav.Link>
+      <ActiveLink href="/home/about" type="navLink"><i className="bi bi-info-square me-2" width="16" height="16"></i> About</ActiveLink>
+      <ActiveLink href="#/orders" type="navLink"><i className="bi bi-table me-2" width="16" height="16"></i> Orders</ActiveLink>
+      <ActiveLink href="#/products" type="navLink"><i className="bi bi-grid me-2" width="16" height="16"></i> Products</ActiveLink>
+      <ActiveLink href="#/customers" type="navLink" disabled><i className="bi bi-person-circle me-2" width="16" height="16"></i> Customers</ActiveLink>
     </Nav>
   )
 }
